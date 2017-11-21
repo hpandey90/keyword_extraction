6218591ea74670734b7e609ffdf691ac689c114f
himanshu <Himanshu Pandey>
2017-11-20 20:06:46 -0500

    data preprocssing done

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,4 +1,5 @@
 import csv as csv
+from bs4 import BeautifulSoup
 def main():
     data_preprocess()
 def data_preprocess():
@@ -10,21 +11,31 @@ def data_preprocess():
     stop_words = set()
     for s in swords:
         for word in s:
-            stop_words.add(word.strip())
+            stop_words.add(word.strip().lower())
     for line in train:
         if len(line)==4:
-            wtrain.writerow((int(line[0]),remove_stop_words(line[1],stop_words),remove_stop_words(line[2],stop_words),line[3]))
-    
+            wtrain.writerow((int(line[0]),remove_stop_words(line[1],stop_words),remove_stop_words(remove_html_tags(line[2]),stop_words),line[3]))
+
 def remove_stop_words(sentence,stop_words):
+    if isinstance(sentence, unicode):
+        sentence = sentence.encode('utf8')
     symbols = ['?',',','.',':',';','+','=','"','/']
     for symbol in symbols:
         sentence = sentence.replace(symbol,' ')
     new_line=""
     for word in sentence.split():
-        if word not in stop_words:
+        if word.lower() not in stop_words:
             new_line+=word+" "
     return new_line
+
+def remove_html_tags(sentence):
+    if isinstance(sentence, unicode):
+        sentence = sentence.encode('utf8')
+    bs = BeautifulSoup(sentence)
+    for tags in ['a','code']:
+        text = bs.findAll(tags)
+        [tag.extract() for tag in text]
+    return bs.get_text()
+
 if __name__=="__main__":
     main()
-
-    
